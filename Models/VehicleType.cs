22c8e829fb9a4aff93cf30df79cb87e62aba507e
Christian Vasquez <christianvasquezmelo@gmail.com>
2019-01-20 10:47:29 -0400

    Make class abstract

--- Models/VehicleType.cs
+++ Models/VehicleType.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RentCar.Models
 {
-    public class VehicleType
+    public abstract class VehicleType
     {
         public int Id { get; set; }
 
