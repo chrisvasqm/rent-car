3307e19408b84d617fdcd8618f0ce71ca0225fe7
Christian Vasquez <christianvasquezmelo@gmail.com>
2019-01-22 14:49:28 -0400

    Feature: Edit a vehicle type

--- Controllers/VehicleTypeController.cs
+++ Controllers/VehicleTypeController.cs
@@ -45,6 +45,7 @@ namespace RentCar.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Save(VehicleType vehicleType)
         {
+            // Show an error message if the state of the model is invalid.
             if (!ModelState.IsValid)
             {
                 var viewModel = new VehicleTypeViewModel(vehicleType)
@@ -55,23 +56,45 @@ namespace RentCar.Controllers
                 return View("VehicleForm", viewModel);
             }
 
+            
             if (vehicleType.Id == 0)
             {
+                // Adding a new item
                 var status = _context.Statuses.Single(m => m.Id == vehicleType.StatusId);
                 vehicleType.Status = status;
                 _context.VehicleTypes.Add(vehicleType);
             }
             else
             {
+                // Editing an existing one
                 var vehicleTypeInDb = _context.VehicleTypes.Single(vt => vt.Id == vehicleType.Id);
 
                 vehicleTypeInDb.Description = vehicleType.Description;
-                vehicleTypeInDb.StatusId = vehicleType.StatusId;                
+                vehicleTypeInDb.StatusId = vehicleType.StatusId;
+
+                var status = _context.Statuses.Single(vt => vt.Id == vehicleTypeInDb.StatusId);
+                vehicleTypeInDb.Status = status;
             }
 
             _context.SaveChanges();
 
             return RedirectToAction("Index", "VehicleType");
+        }
+
+        [Route("vehicle-type/edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            var model = _context.VehicleTypes.SingleOrDefault(m => m.Id == id);
+
+            if (model == null)
+                return NotFound();
+
+            var viewModel = new VehicleTypeViewModel(model)
+            {
+                Statuses = _context.Statuses.ToList(),
+            };
+
+            return View("VehicleForm", viewModel);
         }
     }
 }
