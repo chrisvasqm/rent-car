65a3f10fffedbac3e1d09033e266147687a1d545
Christian Vasquez <christianvasquezmelo@gmail.com>
2019-01-22 14:03:57 -0400

    Feature: Save a new vehicle type

--- Controllers/VehicleTypeController.cs
+++ Controllers/VehicleTypeController.cs
@@ -29,14 +29,49 @@ namespace RentCar.Controllers
             return View(model);
         }
 
+        [Route("vehicle-type/new")]
         public IActionResult New()
         {
-            var form = new VehicleTypeViewModel(new VehicleType())
+            var viewModel = new VehicleTypeViewModel(new VehicleType())
             {
                 Statuses = _context.Statuses.ToList()
             };
 
-            return View("VehicleForm", form);
+            return View("VehicleForm", viewModel);
+        }
+
+        [HttpPost]
+        [Route("vehicle-type/save")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(VehicleType vehicleType)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new VehicleTypeViewModel(vehicleType)
+                {
+                    Statuses = _context.Statuses.ToList()
+                };
+
+                return View("VehicleForm", viewModel);
+            }
+
+            if (vehicleType.Id == 0)
+            {
+                var status = _context.Statuses.Single(m => m.Id == vehicleType.StatusId);
+                vehicleType.Status = status;
+                _context.VehicleTypes.Add(vehicleType);
+            }
+            else
+            {
+                var vehicleTypeInDb = _context.VehicleTypes.Single(vt => vt.Id == vehicleType.Id);
+
+                vehicleTypeInDb.Description = vehicleType.Description;
+                vehicleTypeInDb.StatusId = vehicleType.StatusId;                
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "VehicleType");
         }
     }
 }
