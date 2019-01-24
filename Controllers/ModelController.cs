42b612f120bab9bd3c2cac452ccf3e11307b96a5
Christian Vasquez <christianvasquezmelo@gmail.com>
2019-01-24 11:20:46 -0400

    Feature: Display the list of models

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -1,12 +1,31 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentCar.Models;
 
 namespace RentCar.Controllers
 {
     public class ModelController : Controller
     {
+        private readonly ApplicationContext _context;
+
+        public ModelController()
+        {
+            _context = new ApplicationContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var models = _context.Models
+                .Include(m => m.Status)
+                .ToList();
+
+            return View(models);
         }
     }
 }
