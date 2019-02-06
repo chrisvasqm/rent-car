bfc9a79b4bcc91ac3971ad75148eba548c2e95da
chrisvasqm <christianvasquezmelo@gmail.com>
2019-02-06 09:12:17 -0400

    Feature: Display list of vehicles

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -1,12 +1,23 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using RentCar.Models;
 
 namespace RentCar.Controllers
 {
     public class VehicleController : Controller
     {
+        private readonly ApplicationContext _context;
+        
+        public VehicleController()
+        {
+            _context = new ApplicationContext();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var vehicles = _context.Vehicles.ToList();
+            
+            return View(vehicles);
         }
     }
 }
