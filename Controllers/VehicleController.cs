9862ae984ce3d263b28c87b3c2786cf86bca57f3
chrisvasqm <christianvasquezmelo@gmail.com>
2019-02-09 20:17:00 -0400

    Feature: save a new vehicle

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RentCar.Models;
 
 namespace RentCar.Controllers
@@ -13,13 +14,22 @@ namespace RentCar.Controllers
             _context = new ApplicationContext();
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [Route("vehicles/")]
         public IActionResult Index()
         {
-            var vehicles = _context.Vehicles.ToList();
+            var vehicles = _context.Vehicles
+                .Include(v => v.Status)
+                .ToList();
             
             return View(vehicles);
         }
 
+        [Route("vehicles/new")]
         public IActionResult New()
         {
             var viewModel = new VehicleViewModel(new Vehicle())
@@ -28,6 +38,50 @@ namespace RentCar.Controllers
             };
             
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("vehicles/save")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(Vehicle vehicle)
+        {
+            // Show an error message if the state of the model is invalid.
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new VehicleViewModel(new Vehicle())
+                {
+                    Statuses = _context.Statuses.ToList()
+                };
+
+                return View("New", viewModel);
+            }
+
+            if (vehicle.Id == 0)
+            {
+                // Adding a new item
+                var status = _context.Statuses.SingleOrDefault(s => s.Id == vehicle.StatusId);
+                vehicle.Status = status;
+                _context.Vehicles.Add(vehicle);
+            }
+            else
+            {
+                // Editing an existing one
+                var vehicleInDb = _context.Vehicles.SingleOrDefault(v => v.Id == vehicle.Id);
+
+                if (vehicleInDb == null)
+                    return NotFound();
+
+                vehicleInDb.Description = vehicle.Description;
+                vehicleInDb.StatusId = vehicle.StatusId;
+
+                var status = _context.Statuses.SingleOrDefault(s => s.Id == vehicleInDb.StatusId);
+
+                vehicleInDb.Status = status;
+            }
+
+            _context.SaveChanges();
+            
+            return RedirectToAction("Index", "Vehicle");
         }
     }
 }
