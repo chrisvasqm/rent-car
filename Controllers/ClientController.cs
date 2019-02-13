2c147ccd24014370e9aa25a1f3a9744def0f8e04
chrisvasqm <christianvasquezmelo@gmail.com>
2019-02-13 18:44:46 -0400

    Feature: Save a new client

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using RentCar.Models;
 using RentCar.ViewModel;
@@ -16,6 +17,7 @@ namespace RentCar.Controllers
             _context = new ApplicationContext();
         }
 
+        [Route("clients/")]
         public IActionResult Index()
         {
             var clients = _context.Clients
@@ -40,6 +42,7 @@ namespace RentCar.Controllers
             return RedirectToAction("Index", "Client");
         }
 
+        [Route("clients/details/{id}")]
         public IActionResult Details(int id)
         {
             var client = _context.Clients.SingleOrDefault(c => c.Id == id);
@@ -56,6 +59,7 @@ namespace RentCar.Controllers
             return View(client);
         }
 
+        [Route("clients/new")]
         public IActionResult New()
         {
             var viewModel = new ClientViewModel(new Client())
@@ -66,6 +70,54 @@ namespace RentCar.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("clients/save")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new ClientViewModel(new Client())
+                {
+                    Statuses = _context.Statuses.ToList(),
+                    PersonTypes = _context.PersonTypes.ToList()
+                };
+
+                return View("New", viewModel);
+            }
+
+            if (client.Id == 0)
+            {
+                var status = _context.Statuses.SingleOrDefault(s => s.Id == client.StatusId);
+                client.Status = status;
+
+                var personType = _context.PersonTypes.SingleOrDefault(p => p.Id == client.PersonTypeId);
+                client.PersonType = personType;
+
+                _context.Clients.Add(client);
+            }
+            else
+            {
+                var clientInDb = _context.Clients.SingleOrDefault(c => c.Id == client.Id);
+
+                if (clientInDb == null)
+                    return NotFound();
+
+                clientInDb.Name = client.Name;
+                clientInDb.Status = _context.Statuses.SingleOrDefault(s => s.Id == client.StatusId);
+                clientInDb.StatusId = client.StatusId;
+                clientInDb.PersonType = _context.PersonTypes.SingleOrDefault(p => p.Id == client.PersonTypeId);
+                clientInDb.PersonTypeId = client.PersonTypeId;
+                clientInDb.IdentificationCard = client.IdentificationCard;
+                clientInDb.CreditCard = client.CreditCard;
+                clientInDb.CreditLimit = client.CreditLimit;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Client");
         }
     }
 }
