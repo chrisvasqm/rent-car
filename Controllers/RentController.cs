faccb126c20da5aa7306b95f54a9c61dfd108024
chrisvasqm <christianvasquezmelo@gmail.com>
2019-02-17 17:18:27 -0400

    Add Rent controller

---
+++ Controllers/RentController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using RentCar.Models;
+
+namespace RentCar.Controllers
+{
+    public class RentController : Controller
+    {
+        private readonly ApplicationContext _context;
+        
+        public RentController()
+        {
+            _context = new ApplicationContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
