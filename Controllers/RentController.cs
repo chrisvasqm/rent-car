b11d36e21798a983c587d32b275030694c1851a1
chrisvasqm <christianvasquezmelo@gmail.com>
2019-02-17 17:28:33 -0400

    Feature; Display rents

--- Controllers/RentController.cs
+++ Controllers/RentController.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RentCar.Models;
 
 namespace RentCar.Controllers
@@ -15,6 +17,18 @@ namespace RentCar.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+        public IActionResult Index()
+        {
+            var rents = _context.Rents
+                .Include(r => r.Client)
+                .Include(r => r.Status)
+                .Include(r => r.Vehicle)
+                .Include(r => r.Employee)
+                .ToList();
+
+            return View(rents);
         }
     }
 }
