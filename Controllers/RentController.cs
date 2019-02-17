5661d13a394e900123571a2864c9b98cc57f2b1a
chrisvasqm <christianvasquezmelo@gmail.com>
2019-02-17 18:03:48 -0400

    Add new rent form

--- Controllers/RentController.cs
+++ Controllers/RentController.cs
@@ -66,5 +66,18 @@ namespace RentCar.Controllers
 
             return View(rent);
         }
+
+        public IActionResult New()
+        {
+            var viewModel = new RentViewModel(new Rent())
+            {
+                Statuses = _context.Statuses.ToList(),
+                Clients = _context.Clients.ToList(),
+                Vehicles = _context.Vehicles.ToList(),
+                Employees = _context.Employees.ToList()
+            };
+
+            return View(viewModel);
+        }
     }
 }
--- Models/RentViewModel.cs
+++ Models/RentViewModel.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
 using RentCar.Views.Model;
@@ -9,31 +12,47 @@ namespace RentCar.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Employee")]
         public int EmployeeId { get; set; }
 
         public Employee Employee { get; set; }
 
+        [Display(Name = "Vehicle")]
         public int VehicleId { get; set; }
 
         public Vehicle Vehicle { get; set; }
 
+        [Display(Name = "Client")]
         public int ClientId { get; set; }
 
         public Client Client { get; set; }
-
+        
+        [Display(Name = "Date of rent")]
         public DateTime RentDate { get; set; }
 
+        [Display(Name = "Date of return")]
         public DateTime ReturnDate { get; set; }
 
+        [Display(Name = "Price per day")]
         public int PricePerDay { get; set; }
 
+        [Display(Name = "Number of days")]
         public int NumberOfDays { get; set; }
 
         public string Comment { get; set; }
 
+        [Display(Name = "Status")]
         public byte StatusId { get; set; }
 
         public Status Status { get; set; }
+
+        public IEnumerable<Status> Statuses { get; set; }
+
+        public IEnumerable<Employee> Employees { get; set; }
+
+        public IEnumerable<Client> Clients { get; set; }
+
+        public IEnumerable<Vehicle> Vehicles { get; set; }
         
         public RentViewModel(Rent rent)
         {
